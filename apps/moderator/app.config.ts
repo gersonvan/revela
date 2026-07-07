8503046246a5d7ff72afa3ec581fa23bae50ae42
Gerson Albuquerque <gersonvan@gmail.com>
2026-07-07 12:46:46 -0300

    Link moderator app to EAS project

--- apps/moderator/app.config.ts
+++ apps/moderator/app.config.ts
@@ -11,6 +11,9 @@ const config: ExpoConfig = {
   extra: {
     moderatorApiBaseUrl:
       process.env.EXPO_PUBLIC_MODERATOR_API_BASE_URL ?? "http://127.0.0.1:3000",
+    eas: {
+      projectId: "8d43ff4b-94a6-4c31-83c2-085132a0516d",
+    },
   },
   ios: {
     supportsTablet: false,
